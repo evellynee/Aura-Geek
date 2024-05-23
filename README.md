7c7e867323ab2f9b7d57ded72c29783d35f9e7d7
evellynee <evellynesilva11@gmail.com>
2024-05-23 10:37:45 -0300

    desafio alura

---
+++ README.md
@@ -0,0 +1,40 @@
+<div align="center">
+    <h1>New AluraGeek-Challeng</h1>
+</div>
+
+## Este é o repositório base do nosso projeto, nele voce encontrará uma página  HTML estrutura simples, um arquivo de estilos CSS e um de código Javascript.
+
+- Onde podemos fazer um CRUD(Create, Read, Update, Delete Editar) simples usando JavaScript puro.  
+
+- Com arquivo estlio json junto com main.js busca em API Mockada 
+
+- Onde você pode  ver o projeto: [aqui](https://pedrozef.github.io/new-AluraGeek-Challege/)
+
+## Sobre o desafio utilizamos:
+- HTML
+- CSS
+- Javascript
+- GitHub
+<br></br>
+<div align="center">
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" width="40" height="40" hspace="10">
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" width="40" height="40" hspace="12">
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width="40" height="40" hspace="10">  
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" width="40" height="40" hspace="12">
+</div>
+<br><br>
+
+## Designer do Figma:
+<br><br>
+<div align="center">
+    <img src="project/Desktop.png" width="200" height="100" hspace="25">
+    <br><br>
+     <img src="project/Badge-AluraGeek.png" width="200" height="100" hspace="25">
+</div>
+<br><br>
+
+### Tempo na Execução do Projeto:
+<div align="center">
+    <img src="https://img.shields.io/badge/IN%C3%8DCIO-15%2F05%2F2024-green" hspace="12"/>
+    <img src="https://img.shields.io/badge/T%C3%89RMINO-21%2F05%2F2024-red" hspace="12"/>
+</div>
---
+++ js/main.js
@@ -0,0 +1,138 @@
+let produtos = [
+    {
+        id: 0,
+        imagem: "https://decathlonpro.vtexassets.com/arquivos/ids/2922614/-bola-volei-mikasa-v390w-no-size1.jpg?v=637698243022070000",
+        produto: "Bola 1",
+        valor: 60.0,
+    },
+    {
+        id: 1,
+        imagem: "https://decathlonpro.vtexassets.com/arquivos/ids/2922614/-bola-volei-mikasa-v390w-no-size1.jpg?v=637698243022070000",
+        produto: "Bola 2",
+        valor: 80.0,
+    },
+    {
+        id: 2,
+        imagem: "https://decathlonpro.vtexassets.com/arquivos/ids/2922614/-bola-volei-mikasa-v390w-no-size1.jpg?v=637698243022070000",
+        produto: "Bola 3 ",
+        valor: 120.0,
+    },
+    {
+        id: 3,
+        imagem: "https://decathlonpro.vtexassets.com/arquivos/ids/2922614/-bola-volei-mikasa-v390w-no-size1.jpg?v=637698243022070000",
+        produto: "Bola 4",
+        valor: 60.0,
+    },
+    {
+        id: 4,
+        imagem: "https://decathlonpro.vtexassets.com/arquivos/ids/2922614/-bola-volei-mikasa-v390w-no-size1.jpg?v=637698243022070000",
+        produto: "Bola 5 ",
+        valor: 90.0,
+    },
+    {
+        id: 5,
+        imagem: "https://decathlonpro.vtexassets.com/arquivos/ids/2922614/-bola-volei-mikasa-v390w-no-size1.jpg?v=637698243022070000",
+        produto: "Bola 6",
+        valor: 60.0,
+    },
+    {
+        id: 6,
+        imagem: "https://decathlonpro.vtexassets.com/arquivos/ids/2922614/-bola-volei-mikasa-v390w-no-size1.jpg?v=637698243022070000",
+        produto: "Bola 7",
+        valor: 60.0,
+    },
+    {
+        id: 7,
+        imagem: "https://decathlonpro.vtexassets.com/arquivos/ids/2922614/-bola-volei-mikasa-v390w-no-size1.jpg?v=637698243022070000",
+        produto: "Bola 8 ",
+        valor: 120.0,
+    },
+    {
+        id: 8,
+        imagem: "https://decathlonpro.vtexassets.com/arquivos/ids/2922614/-bola-volei-mikasa-v390w-no-size1.jpg?v=637698243022070000",
+        produto: "Bola 9", 
+        valor: 60.0,
+    },
+];
+
+
+function lerProdutos() {
+    const cards = document.getElementById("cards");
+    cards.innerHTML = "";
+    produtos.forEach((produto) => {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+            <img src="${produto.imagem}" alt="Imagen do produto">
+            <div class="card-container--info">
+                <p>${produto.produto}</p>
+                <div class="card-container--value">
+                    <p>R$ ${produto.valor.toFixed(2)}</p>
+                    <img class="lixo" src="img/lixo.png" alt="Ícone do Lixo" onclick="deleteProduto(${produto.id})">
+                    <img class="editar" src="img/editar.png" alt="Ícone de Edição" onclick="updateProduto(${produto.id})">
+                </div>
+            </div>
+        `;
+        cards.appendChild(card);
+    });
+}
+
+// Função para criar um novo produto
+function createProduto() {
+    const form = document.getElementById("form-produto");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const nome = document.getElementById("nome").value;
+        const valor = parseFloat(document.getElementById("valor").value);
+        const imagem = document.getElementById("imagem").value;
+        if (nome && valor && imagem) {
+            const novoProduto = {
+                id: produtos.length,
+                imagem,
+                produto: nome,
+                valor,
+            };
+            produtos.push(novoProduto);
+            lerProdutos();
+            form.reset();
+        } else {
+            alert("Preencha todos os campos!");
+        }
+    });
+}
+
+// Função para deletar um produto
+function deleteProduto(id) {
+    if (confirm("Tem certeza que deseja excluir o produto?")) {
+        produtos = produtos.filter((produto) => produto.id !== id);
+        lerProdutos();
+        if (produtos.length === 0) {
+            alert("Nenhum produto encontrado!");
+        }
+    }
+}
+
+// Função para atualizar um produto
+function updateProduto(id) {
+    const produto = produtos.find((produto) => produto.id === id);
+    if (produto) {
+        const nome = prompt("Novo nome do produto:");
+        const valor = parseFloat(prompt("Novo valor do produto:"));
+        const imagem = prompt("Nova imagem do produto:");
+        if (nome && valor && imagem) {
+            produto.produto = nome;
+            produto.valor = valor;
+            produto.imagem = imagem;
+            lerProdutos();
+            alert("Produto atualizado com sucesso!");
+        } else {
+            alert("Produto não encontrado!");
+        }
+    }
+}
+
+// Inicializar a leitura dos produtos
+lerProdutos();
+
+// Inicializar a criação de produtos
+createProduto();
